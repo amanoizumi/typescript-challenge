80724c83fb730bb63014b1cb3f73be701a2d8d32
amanoizumi <a0918233246@gmail.com>
2023-12-11 18:25:37 +0800

    update: TS 練習

--- src/arraySum.ts
+++ src/arraySum.ts
@@ -14,4 +14,9 @@
  */
 export function arraySum(numbers: number[]): number {
     // 在此實現函式
+    if(numbers.length === 0) return 0;
+
+    return numbers.reduce((acc: number, curr: number) => {
+        return acc + curr;
+    }, 0)
 }
